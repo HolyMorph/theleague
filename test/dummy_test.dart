41c77e21c3ae033a785ab5c7d4a3e4dc3ac62407
Usukhbayar Ganbaatar <usukhuus@gmail.com>
2022-04-12 16:33:54 +0800

    test added

--- test/dummy_test.dart
+++ test/dummy_test.dart
@@ -1,1 +1,8 @@
-void main() {}
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mezorn_dummy/utils/constants.dart';
+
+void main() {
+  test('IS_debug', () {
+    expect(Constants.isDevUrl, false);
+  });
+}
