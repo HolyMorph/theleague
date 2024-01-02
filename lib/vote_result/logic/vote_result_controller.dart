ac499d07aeeb57a28344be274f963eff77ac2682
Tsogtkhangai <tsogtkhangai@mezorn.com>
2024-01-02 11:56:16 +0800

    bug fixed

--- lib/vote_result/logic/vote_result_controller.dart
+++ lib/vote_result/logic/vote_result_controller.dart
@@ -1,14 +1,10 @@
+import 'dart:convert';
 import 'dart:developer';
-import 'dart:io';
-
-import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:mezorn_api_caller/api_caller.dart';
 
 import '../../service/api_client.dart';
 import '../../storage/local_storage.dart';
-import '../../style/my_colors.dart';
-import '../../utils/basic_utils.dart';
 import '../../utils/constants.dart';
 import '../state/vote_result_state.dart';
 
@@ -102,75 +98,57 @@ class VoteResultController extends GetxController {
     dynamic response = await ApiClient().sendRequest(
       '/api/me/vote-result',
       method: Method.get,
-      queryParam: {
-        'gender': state.isMale.value ? 'male' : 'female',
-        'version': 1.2,
-      },
+      queryParam: {'gender': state.isMale.value ? 'male' : 'female', 'version': 1.2},
     );
 
     if (MezornClientHelper.isValidResponse(response)) {
       List<dynamic> players = await LocalStorage.getData(Constants.META_DATA)['players'];
-      List<dynamic> teams = await LocalStorage.getData(Constants.META_DATA)['teams'];
-      List<dynamic> online = response.data['result']['votes']['online'];
-      List<dynamic> arena = response.data['result']['votes']['arena'];
-      List<dynamic> coach = response.data['result']['votes']['coach'];
 
-      List<dynamic> onlineList = List.from(await LocalStorage.getData(Constants.META_DATA)['players']);
-      List<dynamic> arenaList = List.from(await LocalStorage.getData(Constants.META_DATA)['players']);
-      List<dynamic> coachList = List.from(await LocalStorage.getData(Constants.META_DATA)['players']);
+      List<dynamic> onlineList = [];
+      List<dynamic> arenaList = [];
+      List<dynamic> coachList = [];
 
-      // List<dynamic> onlineList = [];
-      // List<dynamic> arenaList = [];
-      // List<dynamic> coachList = [];
-      //
-      // for (var player in players) {
-      //   onlineList.add(player);
-      //   arenaList.add(player);
-      //   coachList.add(player);
-      // }
+      for (var player in players) {
+        onlineList.add(jsonDecode(jsonEncode(player)));
+      }
+
+      for (var player in players) {
+        arenaList.add(jsonDecode(jsonEncode(player)));
+      }
+
+      for (var player in players) {
+        coachList.add(jsonDecode(jsonEncode(player)));
+      }
 
       /// Online vote
-      for (var index = 0; index < online.length; index++) {
+      for (var index = 0; index < response.data['result']['votes']['online'].length; index++) {
+        log('online');
         for (var playerIndex = 0; playerIndex < onlineList.length; playerIndex++) {
-          if (online[index]['value'] == onlineList[playerIndex]['_id']) {
-            log('online');
-            onlineList[playerIndex]..['score'] = online[index]['score'];
-            for (var teamIndex = 0; teamIndex < teams.length; teamIndex++) {
-              if (teams[teamIndex]['code'] == onlineList[playerIndex]['teamCode']) {
-                state.onlineVoteResults.add(onlineList[playerIndex]);
-              }
-            }
+          if (response.data['result']['votes']['online'][index]['value'] == onlineList[playerIndex]['_id']) {
+            onlineList[playerIndex]..['score'] = response.data['result']['votes']['online'][index]['score'];
+            state.onlineVoteResults.add(onlineList[playerIndex]);
           }
         }
       }
 
       /// Arena vote
-      for (var index = 0; index < arena.length; index++) {
+      for (var index = 0; index < response.data['result']['votes']['arena'].length; index++) {
         log('arena');
         for (var playerIndex = 0; playerIndex < arenaList.length; playerIndex++) {
-          if (arena[index]['value'] == arenaList[playerIndex]['_id']) {
-            arenaList[playerIndex]..['score'] = arena[index]['score'];
-            for (var teamIndex = 0; teamIndex < teams.length; teamIndex++) {
-              if (teams[teamIndex]['code'] == arenaList[playerIndex]['teamCode']) {
-                log('player: ${arenaList[playerIndex]}');
-                state.arenaVoteResults.add(arenaList[playerIndex]);
-              }
-            }
+          if (response.data['result']['votes']['arena'][index]['value'] == arenaList[playerIndex]['_id']) {
+            arenaList[playerIndex]..['score'] = response.data['result']['votes']['arena'][index]['score'];
+            state.arenaVoteResults.add(arenaList[playerIndex]);
           }
         }
       }
 
       /// Coach
-      for (var index = 0; index < coach.length; index++) {
+      for (var index = 0; index < response.data['result']['votes']['coach'].length; index++) {
         for (var playerIndex = 0; playerIndex < coachList.length; playerIndex++) {
-          if (coach[index]['value'] == coachList[playerIndex]['_id']) {
+          if (response.data['result']['votes']['coach'][index]['value'] == coachList[playerIndex]['_id']) {
             log('coach');
-            coachList[playerIndex]..['score'] = coach[index]['score'];
-            for (var teamIndex = 0; teamIndex < teams.length; teamIndex++) {
-              if (teams[teamIndex]['code'] == coachList[playerIndex]['teamCode']) {
-                state.coachVoteResults.add(coachList[playerIndex]);
-              }
-            }
+            coachList[playerIndex]..['score'] = response.data['result']['votes']['coach'][index]['score'];
+            state.coachVoteResults.add(coachList[playerIndex]);
           }
         }
       }
@@ -191,75 +169,75 @@ class VoteResultController extends GetxController {
     await getVoteHistory();
   }
 
-  void showForceUpdateDialog() {
-    Get.dialog(
-      PopScope(
-        canPop: false,
-        child: AlertDialog(
-          contentPadding: EdgeInsets.zero,
-          backgroundColor: Color(0xFF272739),
-          content: Container(
-            padding: const EdgeInsets.fromLTRB(16, 0, 16, 16),
-            width: double.infinity,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(8),
-              border: Border.all(width: 1, color: Colors.white.withOpacity(0.1)),
-              color: Color(0xFF272739),
-            ),
-            child: Stack(
-              alignment: Alignment.bottomCenter,
-              children: [
-                Positioned(
-                  top: -75,
-                  child: Image.asset('assets/images/ic_dialog_body.png', scale: 3),
-                ),
-                Column(
-                  mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const SizedBox(height: 100),
-                    Text(
-                      'Апп дээр шинэчлэлт гарсан байгаа тул та татаж авна уу. Баярлалаа.',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600, fontFamily: 'GIP', color: Colors.white),
-                    ),
-                    const SizedBox(height: 24),
-                    Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Expanded(
-                          child: ElevatedButton(
-                            style: ElevatedButton.styleFrom(backgroundColor: Color(0xFF686874)),
-                            onPressed: () => Get.back(),
-                            child: Text(
-                              'Үгүй',
-                              style: TextStyle(fontFamily: 'GIP', fontWeight: FontWeight.w600),
-                            ),
-                          ),
-                        ),
-                        const SizedBox(width: 8),
-                        Expanded(
-                          child: ElevatedButton(
-                            style: ElevatedButton.styleFrom(backgroundColor: MyColors.secondaryColor),
-                            onPressed: () => BasicUtils().urlLaunch(
-                              '${Platform.isIOS ? 'https://apps.apple.com/us/app/thele%D0%B0gue/id6474849692' : 'https://play.google.com/store/apps/details?id=mn.mezorn.duiz'}',
-                            ),
-                            child: Text(
-                              'Тийм',
-                              style: TextStyle(fontFamily: 'GIP', fontWeight: FontWeight.w600),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-      barrierDismissible: false,
-    );
-  }
+  // void showForceUpdateDialog() {
+  //   Get.dialog(
+  //     PopScope(
+  //       canPop: false,
+  //       child: AlertDialog(
+  //         contentPadding: EdgeInsets.zero,
+  //         backgroundColor: Color(0xFF272739),
+  //         content: Container(
+  //           padding: const EdgeInsets.fromLTRB(16, 0, 16, 16),
+  //           width: double.infinity,
+  //           decoration: BoxDecoration(
+  //             borderRadius: BorderRadius.circular(8),
+  //             border: Border.all(width: 1, color: Colors.white.withOpacity(0.1)),
+  //             color: Color(0xFF272739),
+  //           ),
+  //           child: Stack(
+  //             alignment: Alignment.bottomCenter,
+  //             children: [
+  //               Positioned(
+  //                 top: -75,
+  //                 child: Image.asset('assets/images/ic_dialog_body.png', scale: 3),
+  //               ),
+  //               Column(
+  //                 mainAxisSize: MainAxisSize.min,
+  //                 mainAxisAlignment: MainAxisAlignment.center,
+  //                 children: [
+  //                   const SizedBox(height: 100),
+  //                   Text(
+  //                     'Апп дээр шинэчлэлт гарсан байгаа тул та татаж авна уу. Баярлалаа.',
+  //                     textAlign: TextAlign.center,
+  //                     style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600, fontFamily: 'GIP', color: Colors.white),
+  //                   ),
+  //                   const SizedBox(height: 24),
+  //                   Row(
+  //                     mainAxisSize: MainAxisSize.min,
+  //                     children: [
+  //                       Expanded(
+  //                         child: ElevatedButton(
+  //                           style: ElevatedButton.styleFrom(backgroundColor: Color(0xFF686874)),
+  //                           onPressed: () => Get.back(),
+  //                           child: Text(
+  //                             'Үгүй',
+  //                             style: TextStyle(fontFamily: 'GIP', fontWeight: FontWeight.w600),
+  //                           ),
+  //                         ),
+  //                       ),
+  //                       const SizedBox(width: 8),
+  //                       Expanded(
+  //                         child: ElevatedButton(
+  //                           style: ElevatedButton.styleFrom(backgroundColor: MyColors.secondaryColor),
+  //                           onPressed: () => BasicUtils().urlLaunch(
+  //                             '${Platform.isIOS ? 'https://apps.apple.com/us/app/thele%D0%B0gue/id6474849692' : 'https://play.google.com/store/apps/details?id=mn.mezorn.duiz'}',
+  //                           ),
+  //                           child: Text(
+  //                             'Тийм',
+  //                             style: TextStyle(fontFamily: 'GIP', fontWeight: FontWeight.w600),
+  //                           ),
+  //                         ),
+  //                       ),
+  //                     ],
+  //                   ),
+  //                 ],
+  //               ),
+  //             ],
+  //           ),
+  //         ),
+  //       ),
+  //     ),
+  //     barrierDismissible: false,
+  //   );
+  // }
 }
--- lib/vote_result/state/vote_result_state.dart
+++ lib/vote_result/state/vote_result_state.dart
@@ -1,11 +1,11 @@
 import 'package:get/get.dart';
 
 class VoteResultState {
-  RxList<dynamic> arenaVoteResults = RxList([]);
-  RxList<dynamic> coachVoteResults = RxList([]);
+  RxList<dynamic> arenaVoteResults = RxList();
+  RxList<dynamic> coachVoteResults = RxList();
   RxString gender = 'male'.obs;
   RxBool isLoading = true.obs;
   RxBool isMale = true.obs;
-  RxList<dynamic> onlineVoteResults = RxList([]);
-  RxList<Map<String, dynamic>> voteHistories = RxList([]);
+  RxList<dynamic> onlineVoteResults = RxList();
+  RxList<Map<String, dynamic>> voteHistories = RxList();
 }
